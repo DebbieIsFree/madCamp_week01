1908c7adbc33ad3b00deff10963df900a02ac826
wldnj <wldnjs980409@naver.com>
2022-07-02 10:14:28 +0900

    change -> tab Layout

--- app/src/main/java/com/example/madcamp/MainActivity.kt
+++ app/src/main/java/com/example/madcamp/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.madcamp
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,25 +11,65 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val phoneBtn = findViewById<Button>(R.id.phone)
-        val galleryBtn = findViewById<Button>(R.id.gallery)
-        val mapBtn = findViewById<Button>(R.id.map)
+//        val phoneBtn = findViewById<Button>(R.id.phone)
+//        val galleryBtn = findViewById<Button>(R.id.gallery)
+//        val mapBtn = findViewById<Button>(R.id.map)
 
-        phoneBtn.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.frame, PhoneFragment())
-                .addToBackStack(null)
-                .commit()
-        }
-        galleryBtn.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.frame, GalleryFragment())
-                .commit()
-        }
-        mapBtn.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.frame, MapViewFragment())
-                .commit()
-        }
+//        phoneBtn.setOnClickListener {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.frame, PhoneFragment())
+//                .addToBackStack(null)
+//                .commit()
+//        }
+//        galleryBtn.setOnClickListener {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.frame, GalleryFragment())
+//                .commit()
+//        }
+//        mapBtn.setOnClickListener {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.frame, MapViewFragment())
+//                .commit()
+//        }
+
+
+
+
+        /////
+
+
+        var fragment1 = PhoneFragment()
+        var fragment2 = GalleryFragment()
+        var fragment3 = MapViewFragment()
+
+        val  tabs = findViewById<TabLayout>(R.id.tabs)
+        tabs.addTab(tabs.newTab().setText("친구"))
+        tabs.addTab(tabs.newTab().setText("채팅"))
+        tabs.addTab(tabs.newTab().setText("더보기"))
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frame, fragment1).commit()
+
+
+        tabs.setOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                var position = tab.getPosition()
+                lateinit var selected : Fragment
+                if (position == 0)
+                    selected = fragment1
+                else if (position == 1)
+                    selected = fragment2
+                else if (position == 2)
+                    selected = fragment3
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, selected)
+                    .commit()
+            }
+        })
+
     }
 }
--- app/src/main/java/com/example/madcamp/MapViewFragment.kt
+++ app/src/main/java/com/example/madcamp/MapViewFragment.kt
@@ -1,15 +1,23 @@
 package com.example.madcamp
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
 import androidx.annotation.UiThread
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.naver.maps.map.MapView
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import androidx.fragment.app.FragmentActivity
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
 
 class MapViewFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mapView: MapView
@@ -70,4 +78,112 @@ class MapViewFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(naverMap: NaverMap) {
         // ...
     }
+
+
+
+
+
+
+    /////
+
+//    private lateinit var fusedLocationClient: FusedLocationProviderClient
+//    val permission_request = 99
+//    private lateinit var naverMap: NaverMap
+//    var permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+//    //권한 가져오기
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        //activity가 최초 실행 되면 이곳을 수행
+//
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.frag_map)
+//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//
+//        if (isPermitted()) {
+//            startProcess()
+//        } else {
+//            ActivityCompat.requestPermissions(this, permissions, permission_request)
+//        }//권한 확인
+//    }
+//
+//    fun isPermitted(): Boolean {
+//        for (perm in permissions) {
+//            if (ContextCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED) {
+//                return false
+//            }
+//        }
+//        return true
+//    }//권한을 허락 받아야함
+//
+//    fun startProcess(){
+//        val fm = supportFragmentManager
+//        val mapFragment = fm.findFragmentById(R.id.map_view) as MapFragment?
+//            ?: MapFragment.newInstance().also {
+//                fm.beginTransaction().add(R.id.map_view, it).commit()
+//            } //권한
+//        mapFragment.getMapAsync(this)
+//    } //권한이 있다면 onMapReady연결
+//
+//
+//    @UiThread
+//    override fun onMapReady(naverMap: NaverMap){
+//
+//        val cameraPosition = CameraPosition(
+//            LatLng(37.5666102, 126.9783881),  // 위치 지정
+//            16.0 // 줌 레벨
+//        )
+//        naverMap.cameraPosition = cameraPosition
+//        this.naverMap = naverMap
+//
+//        fusedLocationProviderClient =
+//            LocationServices.getFusedLocationProviderClient(this) //gps 자동으로 받아오기
+//        setUpdateLocationListner() //내위치를 가져오는 코드
+//    }
+//
+//    //내 위치를 가져오는 코드
+//    lateinit var fusedLocationProviderClient: FusedLocationProviderClient //자동으로 gps값을 받아온다.
+//    lateinit var locationCallback: LocationCallback //gps응답 값을 가져온다.
+//
+//    @SuppressLint("MissingPermission")
+//    fun setUpdateLocationListner() {
+//        val locationRequest = LocationRequest.create()
+//        locationRequest.run {
+//            var priority = LocationRequest.PRIORITY_HIGH_ACCURACY //높은 정확도
+//            var interval = 1000 //1초에 한번씩 GPS 요청
+//        }
+//
+//        locationCallback = object : LocationCallback() {
+//            override fun onLocationResult(locationResult: LocationResult?) {
+//                locationResult ?: return
+//                for ((i, location) in locationResult.locations.withIndex()) {
+//                    Log.d("location: ", "${location.latitude}, ${location.longitude}")
+//                    setLastLocation(location)
+//                }
+//            }
+//        }
+//        //location 요청 함수 호출 (locationRequest, locationCallback)
+//
+//        fusedLocationProviderClient.requestLocationUpdates(
+//            locationRequest,
+//            locationCallback,
+//            Looper.myLooper()
+//        )
+//    }//좌표계를 주기적으로 갱신
+//
+//    fun setLastLocation(location: Location) {
+//        val myLocation = LatLng(location.latitude, location.longitude)
+//        val marker = Marker()
+//        marker.position = myLocation
+//
+//        marker.map = naverMap
+//        //마커
+//        val cameraUpdate = CameraUpdate.scrollTo(myLocation)
+//        naverMap.moveCamera(cameraUpdate)
+//        naverMap.maxZoom = 18.0
+//        naverMap.minZoom = 5.0
+//
+//        //marker.map = null
+//    }
+
+
 }
