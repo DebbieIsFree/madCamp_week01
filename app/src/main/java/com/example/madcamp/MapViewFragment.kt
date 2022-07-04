128d6208845b2f728b8008eabb225fc71303e38b
wldnj <wldnjs980409@naver.com>
2022-07-04 17:00:06 +0900

    Map - print current location & marker

--- app/src/main/java/com/example/madcamp/MapViewFragment.kt
+++ app/src/main/java/com/example/madcamp/MapViewFragment.kt
@@ -1,42 +1,104 @@
 package com.example.madcamp
 
 
+import android.Manifest
 import android.content.Context
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.location.*
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import com.naver.maps.map.LocationTrackingMode
-import com.naver.maps.map.MapView
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
+import com.naver.maps.map.*
 import com.naver.maps.map.util.FusedLocationSource
-
-
-private lateinit var mapView: MapView
-lateinit var mcontext: Context
+import java.util.*
 
 
 class MapViewFragment : Fragment(), OnMapReadyCallback {
 
-    val LOCATION_PERMISSION_REQUEST_CODE = 1000
     private lateinit var locationSource: FusedLocationSource
-    private lateinit var naverMap: NaverMap
+    private lateinit var neverMap: NaverMap
+    private lateinit var mapView: MapView
+    private lateinit var mcontext: Context
+
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
+    private lateinit var locationManager : LocationManager
+
+    private var lat : Double = 0.0
+    private var lon : Double = 0.0
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
+
+//    private val locationRequest: LocationRequest? = null
+
+
+//    private val permissions = arrayOf(
+//        Manifest.permission.ACCESS_FINE_LOCATION,
+//        Manifest.permission.ACCESS_COARSE_LOCATION
+//    )
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-
         mcontext = context as MainActivity
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+
         locationSource =
             FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+        locationManager = mcontext.getSystemService(LOCATION_SERVICE) as LocationManager
+
+        if (ActivityCompat.checkSelfPermission(
+                mcontext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                mcontext,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
+            override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
+
+            override fun isCancellationRequested() = false
+        })
+            .addOnSuccessListener { location: Location? ->
+                if (location == null)
+                    Log.d("Failed","Cannot_get_location")
+                else {
+                    lat = location.latitude
+                    lon = location.longitude
+                    Log.d("location : ", "${lat}, $lon")
+                }
+
+                val geocoder  = Geocoder(mcontext, Locale.KOREA)
+                var addr = geocoder.getFromLocation(lat, lon, 1)
+
+                Log.d("주소", "${addr[0].countryName}")
+                Log.d("주소", "${addr[0].subLocality}")
+                Log.d("주소", "${addr[0].subAdminArea}")
+            }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -52,7 +114,6 @@ class MapViewFragment : Fragment(), OnMapReadyCallback {
         mapView.onCreate(savedInstanceState)
         mapView.onResume()
 //        mapView.getMapAsync(this)
-
     }
 
     override fun onStart() {
@@ -90,11 +151,11 @@ class MapViewFragment : Fragment(), OnMapReadyCallback {
         mapView.onLowMemory()
     }
 
-
     override fun onDestroyView() {
         super.onDestroyView()
         mapView.onDestroy()
     }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -102,32 +163,23 @@ class MapViewFragment : Fragment(), OnMapReadyCallback {
         grantResults: IntArray
     ) {
 
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (locationSource.onRequestPermissionsResult(
-                requestCode, permissions,
-                grantResults
-            )
-        ) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults))
+        {
             if (!locationSource.isActivated) { // 권한 거부됨
-                naverMap.locationTrackingMode = LocationTrackingMode.None
+                neverMap.locationTrackingMode = LocationTrackingMode.None
+                return
             }
-            return
+            else{
+                neverMap.locationTrackingMode = LocationTrackingMode.Follow
+            }
         }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-
     override fun onMapReady(naverMap: NaverMap) {
-        this.naverMap = naverMap
+        this.neverMap = naverMap
         naverMap.locationSource = locationSource
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
-
-        naverMap.addOnLocationChangeListener { location ->
-//            Toast.makeText(this, "${location.latitude}, ${location.longitude}",
-//                Toast.LENGTH_SHORT).show()
-            Log.d("Success : ", "SUCCESS")
-            Log.d("loc : ", "${location.latitude}, ${location.longitude}")
-        }
     }
 
 }
